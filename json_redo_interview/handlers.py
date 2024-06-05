d67918dea36d94923517cde6858bc2e369ad9e40
Francesco Milani <francesco_milani@outlook.com>
2024-06-05 16:55:46 +0300

    refactor handler trigger

--- json_redo_interview/handlers.py
+++ json_redo_interview/handlers.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
-from inspect import Traceback
-from typing import TYPE_CHECKING, Self
+from typing import TYPE_CHECKING
 
 from loguru import logger
 from pydantic import ValidationError
@@ -13,46 +12,36 @@ if TYPE_CHECKING:
 
 
 class HandlerBase(ABC):
-    def __init__(self, db_session: "FakeDB", data: dict[str, str | None]) -> None:
+    def __init__(self, data: dict[str, str | None]) -> None:
         self.data = data
-        self.db_session = db_session
 
     @abstractmethod
     def send(self) -> None:
         pass
 
-    def __enter__(self) -> Self:
-        return self
-
-    def __exit__(self, exc_type: type[Exception] | None, exc_val: Exception | None, exc_tb: Traceback | None) -> None:
-        if exc_val:
-            self.db_session.insert_failed(self.data, reason=str(exc_val))
-        else:
-            self.db_session.increase_processed()
-
 
 class SMSHandler(HandlerBase):
-    def __init__(self, db_session: "FakeDB", data: dict[str, str | None]) -> None:
+    def __init__(self, data: dict[str, str | None]) -> None:
         self.phone = data["phone"]
-        super().__init__(db_session, data)
+        super().__init__(data)
 
     def send(self) -> None:
         logger.info("SMS sent to {}. Data: {}", self.phone, self.data)
 
 
 class EmailHandler(HandlerBase):
-    def __init__(self, db_session: "FakeDB", data: dict[str, str | None]) -> None:
+    def __init__(self, data: dict[str, str | None]) -> None:
         self.email = data["email"]
-        super().__init__(db_session, data)
+        super().__init__(data)
 
     def send(self) -> None:
         logger.info("Email sent to {}. Data: {}", self.email, self.data)
 
 
 class PostHandler(HandlerBase):
-    def __init__(self, db_session: "FakeDB", data: dict[str, str | None]) -> None:
+    def __init__(self, data: dict[str, str | None]) -> None:
         self.url = data["url"]
-        super().__init__(db_session, data)
+        super().__init__(data)
 
     def send(self) -> None:
         logger.info("POST sent to {}. Data: {}", self.url, self.data)
@@ -68,12 +57,10 @@ SENDER_MAP: dict[EventTypes, type[HandlerBase]] = {
 def process_event(event: dict, db_session: "FakeDB") -> None:
     try:
         validated = EventSchema(**event)
-    except ValidationError as e:
+        handler = SENDER_MAP[validated.event_type]
+        serialized_payload = validated.model_dump()
+        handler(serialized_payload).send()
+        db_session.increase_processed()
+    except (ValidationError, KeyError) as e:
         db_session.insert_failed(event, reason=str(e))
         return
-
-    if handler := SENDER_MAP.get(validated.event_type):
-        with handler(db_session, validated.model_dump()) as sender:
-            sender.send()
-    else:
-        db_session.insert_failed(event, reason=f"Unknown event type '{validated.event_type}'")
